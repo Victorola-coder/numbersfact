d8146f596de5610bdb9aa26bf6b108aa11f0fc43
Victorola <victoluolatunji@gmail.com>
2024-08-23 10:17:51 -0700

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+let fact = document.querySelector("#facts");
+let factInput = document.querySelector("#factInput");
+let factOutput = document.querySelector("#factsout");
+
+factInput.addEventListener("input", getFacts);
+
+function getFacts() {
+  let number = factInput.value;
+
+  fetch("http://numbersapi.com/" + number)
+    .then((response) => response.text())
+    .then((data) => {
+      if (number !== "") {
+        // console.log(data);
+        fact.style.display = "block";
+        factOutput.innerHTML = data;
+      }
+    })
+    .catch((err) => console.log(err));
+}
